678425f195f364085129a7e2fc51f4a62632932c
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-21 13:17:06 +0100

    implement routing with react-router-dom in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}>
+          Product
+        </Route>
+        <Route path="product" element={<Product />}>
+          Product
+        </Route>
+        <Route path="pricing" element={<Pricing />}>
+          Product
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
