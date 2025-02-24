795e256636bfe876401bc20992f5ea990810f531
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-24 10:25:04 +0100

    add Footer component with styles from Footer.module.css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,14 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
+import Footer from "./Footer/Footer";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          &copy; Copyrigth {new Date().getFullYear()} by Unicornes Inc.
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 }
