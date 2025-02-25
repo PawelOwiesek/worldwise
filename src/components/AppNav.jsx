0b7658037d771c2394c81c3668f70fdec7275bd5
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-25 09:34:09 +0100

    add links to navigate between pages

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
   return (
-    <nav>
-      <ul className={styles.nav}>
-        <Link to="/">home</Link>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
       </ul>
     </nav>
   );
