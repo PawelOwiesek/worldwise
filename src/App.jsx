103df48d203dc23810c80b1bb337b101388ff83a
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-25 09:33:28 +0100

    add nested routes to AppLayout navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,23 @@ import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import City from "./components/City";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}>
-          Product
-        </Route>
-        <Route path="pricing" element={<Pricing />}>
-          Product
-        </Route>
-        <Route path="product" element={<Product />}>
-          Product
-        </Route>
-        <Route path="login" element={<Login />}>
-          Product
-        </Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          App
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
         </Route>
-        <Route path="*" element={<PageNotFound />}>
-          App
-        </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
