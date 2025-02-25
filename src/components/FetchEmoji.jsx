a0f1942a94b7a86fb3c44fc158f3e4ec3ac2d1d5
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-25 16:52:49 +0100

    fix typo in import path

--- src/components/FetchEmoji.jsx
+++ src/components/FetchEmoji.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styles from "./fetchEmoji.module.css";
+import styles from "./FetchEmoji.module.css";
 
 import emojiToCountryCode from "../../data/emojiToCountryCode";
 
