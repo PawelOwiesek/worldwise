99841e318bd7b5c67e9d2ed57263a8bbcf2d43c0
Pawel Owiesek <Owiesek.p@gmail.com>
2025-02-26 14:24:49 +0100

    firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_APP_FIREBASE_AUTH_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_API_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_API_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export const getCities = async () => {
+  const querySnapshot = await getDocs(collection(db, "cities"));
+  return querySnapshot.docs.map((doc) => doc.data());
+};
+
+export { db };
