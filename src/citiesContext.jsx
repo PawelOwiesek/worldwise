c1cab6015c549752d86458229d1231e391cbfc5a
Pawel Owiesek <Owiesek.p@gmail.com>
2025-03-03 18:25:49 +0100

    add currentCity state variable and fetch function getCity to fetch city on its id

--- src/citiesContext.jsx
+++ src/citiesContext.jsx
@@ -6,6 +6,7 @@ const CitiesContext = createContext([]);
 const CitiesProvider = ({ children }) => {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchData() {
@@ -22,8 +23,20 @@ const CitiesProvider = ({ children }) => {
     fetchData();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await getCities(id);
+      setCurrentCity(res);
+    } catch (error) {
+      alert(`There was an error loading data... ${error}`);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity, getCity }}>
       {children}
     </CitiesContext.Provider>
   );
