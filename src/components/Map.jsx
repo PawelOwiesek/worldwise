88810fd22b35772846e3b6af26514fad2b77f617
Pawel Owiesek <Owiesek.p@gmail.com>
2025-03-04 16:32:13 +0100

    The updated Map component now integrates geolocation, allowing users to set the map center to their current position and providing a button to trigger location retrieval.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,29 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
-import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import useCities from "../useCities";
+import { useGeolocation } from "../useGeoLocation";
 import FetchEmoji from "./FetchEmoji";
+import styles from "./Map.module.css";
+import Button from "./Button";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 
 function Map() {
-  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [mapPositon, setMapPositon] = useState([40, 0]);
   const { cities } = useCities();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -17,8 +31,18 @@ function Map() {
     if (mapLat && mapLng) setMapPositon([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+  useEffect(() => {
+    if (geoLocationPosition)
+      setMapPositon([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
+
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <div className={styles.mapContainer}>
+      {!geoLocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPositon}
@@ -41,6 +65,7 @@ function Map() {
           </Marker>
         ))}
         <ChangeCenter position={mapPositon} />
+        <OnMapClick />
       </MapContainer>
     </div>
   );
@@ -50,6 +75,16 @@ function ChangeCenter({ position }) {
   const map = useMap();
   map.setView(position);
   return null;
+}
+
+function OnMapClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
